f1d2a0884bb64d2298828adbd245f357ccc678b3
israice <israice@gmail.com>
2026-01-26 03:28:55 +0200

    v0.0.3 - testing server webhook test9

--- DASHBOARD/deploy.sh
+++ DASHBOARD/deploy.sh
@@ -4,6 +4,9 @@ set -e
 echo "=== Deploy started ==="
 cd /repo
 
+# Разрешить git работать с репозиторием другого владельца
+git config --global --add safe.directory /repo
+
 echo "Fetching latest changes..."
 git fetch origin master
 
